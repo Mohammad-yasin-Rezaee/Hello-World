9aac39e969cab25718ef1cc66fa8e8cb0695dacb
Mohammad yasin <152398915+Mohammad-yasin-Rezaee@users.noreply.github.com>
2023-11-29 16:48:02 +0330

    Add files via upload

---
+++ Test 8.py
@@ -0,0 +1,10 @@
+a = int( input("Enter your number: "))
+b = int( input("Enter your number: "))
+c = int ( input("Enter your number: "))
+
+if a + b > c and a + c > b and b + c > a:
+
+    print("That's true")
+
+else:
+    print("That's false")
---
+++ Test7.py
@@ -0,0 +1,15 @@
+fg=input("Enter fg(+,-):")
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+
+if fg=="+":
+    print(a+b)
+
+if fg=="-":
+    print(a-b)
+
+if fg=="/":
+    print(a/b)
+
+if fg=="*":
+    print(a*b)
